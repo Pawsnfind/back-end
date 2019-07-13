162824708ece7c3de920a044a6ed515334b62988
Ming Liu <mliu@macpro.home>
2019-07-13 15:23:37 -0400

    add testing model and route

---
+++ routes/animals.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const Animals = require('../models/animals.js');
+
+
+router.get('/', (req, res) => {
+    Animals.getAll()
+    .then( animals => {
+        res.status(200).json(animals)
+    })
+    .catch( error => {
+        res.status(500).json({ message: "Error getting animals", error: error.toString()})
+    })
+})
+
+
+module.exports = router;
