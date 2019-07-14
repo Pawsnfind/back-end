164444fbfe185e82e162571d8dda36248631ee29
Ming Liu <mliu@macpro.home>
2019-07-14 17:27:01 -0400

    add getById function for full application display

--- models/applications/applications.js
+++ models/applications/applications.js
@@ -1,4 +1,5 @@
 const db = require('../../data/dbConfig')
+const knex = require('knex')
 
 module.exports = {
     getAll,
@@ -18,21 +19,22 @@ function getAll() {
 // get by application id
 function getById(id) {
     let query = db
-    .select('applications.id', 'animals.name', 'shelters.shelter', 'application_status.application_status', 'applications.user_id') 
+    .select('applications.id', 'animals.name', 'shelters.shelter', 'application_status.application_status', 'applications.user_id', 'application_meta.*') 
     .from('applications')
     .innerJoin('animals', 'applications.animal_id', 'animals.id')
     .innerJoin('shelters', 'applications.shelter_id', 'shelters.id')
     .innerJoin('application_status', 'applications.application_status_id', 'application_status.id')
+    .innerJoin('application_meta', 'applications.id', 'application_meta.application_id')
 
     if(id) {
         query.where('applications.id', id).first()
-        const promises = [query, getApplicationMeta(id), getApplicationNotes(id)]
+        const promises = [query, getApplicationNotes(id)]
 
         return Promise.all(promises).then( results => {
-            let [application, meta, notes] = results;
+            let [application, notes] = results;
 
             if(application) {
-                application.meta = meta;
+                
                 application.notes = notes;
 
                 return application
@@ -46,15 +48,51 @@ function getById(id) {
 
 }
 
-function getApplicationMeta(application_id) {
-    return db('application_meta')
-    .where({ application_id })
-    .first()
+function getApplicationNotes(application_id) {
+    if(application_id) {
+        let query = db
+        .select('application_admin.notes', 'application_admin.shelter_user_id as by', 'application_admin.created_at')
+        .from('application_admin')
+        .innerJoin('shelter_users', 'application_admin.shelter_user_id', 'shelter_users.id')
+        .where('application_admin.application_id', application_id)
+        
+        let promises = [query, getShelterUserName(query.by)]
+
+        return Promise.all(promises).then( results => {
+            let [notes, username] = results
+
+            notes.by = username
+            return notes
+        })
+
+        /*
+        let shelterUserName = db
+        .select('name')
+        .from('user_meta')
+        .where('shelter_user_id', query.by)
+        .first()
+        
+        query.by = shelterUserName
+
+        return query*/
+        
+    } else {
+        return null
+    }
+    
 }
 
-function getApplicationNotes(application_id) {
-    return db('application_admin')
-    .where({ application_id })
+
+function getShelterUserName(shelter_user_id) {
+    if(shelter_user_id) {
+        return db
+        .select('name')
+        .from('user_meta')
+        .where({shelter_user_id})
+        .first()
+    } else {
+        return null
+    }
 }
 
 
