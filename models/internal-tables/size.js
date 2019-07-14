3c44c8c7adddead50d2ae3a7f44be1d1f645a99c
Lenna <lenna@lennamau.com>
2019-07-14 16:00:29 -0700

    size table models completed

--- models/internal-tables/size.js
+++ models/internal-tables/size.js
@@ -1,2 +1,44 @@
 const db = require('../../data/dbConfig')
 
+module.exports = {
+    getAll,
+    getBy,
+    getById,
+    add,
+    remove,
+    update
+}
+
+function getAll() {
+    return db('size')
+}
+
+function getById(id) {
+    return db('size')
+    .where({ id })
+    .first()
+}
+
+function getBy(filter) {
+    return db('size')
+    .where(filter)
+}
+
+function add(state) {
+    return db('size')
+    .insert(state, 'id')
+    .then(([id]) => getById(id))
+}
+
+function remove(id) {
+    return db('size')
+    .where({ id })
+    .del()
+} 
+
+function update(id, change) {
+    return db('size')
+    .where({id})
+    .update(change)
+    .then(updatedSize => updatedSize ? updatedSize : null)
+}
