6d0787b7ab8ae841632d7869929024aa3b55ea76
ghungpham <pham.ghungj@gmail.com>
2019-07-16 21:48:52 -0400

    changed

--- routes/donations.js
+++ routes/donations.js
@@ -14,14 +14,18 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res)=> {
 
     const { id } = req.params;
-
-    Donations.getDonationbyId(id)
-    .then(donation => {
-        res.status(200).json(donation)
-    })
-    .catch(err => {
-        res.status(500).json({ message: "Error getting donation", err: err.toString() })
-    })
+        Donations.getDonationbyId(id)
+        .then(donation => {
+            if (req.params.id){
+            res.status(200).json(donation)
+            } else { 
+                res.status(404).json({ message:'id cannot be found '})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Error getting donation", err: err.toString() })
+        })
+    
 })
 
 router.get('/:id/user', (req, res)=> {
@@ -30,7 +34,11 @@ router.get('/:id/user', (req, res)=> {
 
     Donations.getDonationsByUser(id)
     .then(donation => {
-        res.status(200).json(donation)
+        if (req.params.id){
+            res.status(200).json(donation)
+            } else { 
+                res.status(404).json({ message:'id cannot be found '})
+            }
     })
     .catch(err => {
         res.status(500).json({ message: "Error getting donation", err: err.toString() })
@@ -43,7 +51,11 @@ router.get('/:id/shelter', (req, res)=> {
 
     Donations.getDonationsByShelter(id)
     .then(donation => {
-        res.status(200).json(donation)
+        if (req.params.id){
+            res.status(200).json(donation)
+            } else { 
+                res.status(404).json({ message:'id cannot be found '})
+            }
     })
     .catch(err => {
         res.status(500).json({ message: "Error getting donation", err: err.toString() })
@@ -54,14 +66,18 @@ router.post('/', (req, res) => {
     
     const { user_id, shelter_id, amount } = req.body
 
-    Donations.addDonation({ user_id, shelter_id, amount })
-    .then(donation => {
-        res.status(201).json({ message: "Successfully donated", donation})
-    })
-    .catch(err => {
-        res.status(500).json({ message: "Error donating", err: err.toString() })
-    })
-    
+    if( req.body.user_id && req.body.shelter_id && req.body.amount){
+
+        Donations.addDonation({ user_id, shelter_id, amount })
+        .then(donation => {
+            res.status(201).json({ message: "Successfully donated", donation})
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Error donating", err: err.toString() })
+        })    
+    } else {
+        res.status(400).json({ message: 'Required fields are incomplete' })
+    }
 })
 
 router.put('/:id', (req, res ) => {
@@ -71,7 +87,11 @@ router.put('/:id', (req, res ) => {
     
     Donations.updateDonation(id, { user_id, shelter_id, amount } )
     .then(donation => {
+        if (req.params.id){
         res.status(200).json({ message: "Successfully updated", donation})
+        } else {
+            res.status(404).json({ message: "id cannot be found"})
+        }
     })
     .catch(err => {
         res.status(500).json({ message: "Error getting updated", err: err.toString() })
--- routes/subscriptions.js
+++ routes/subscriptions.js
@@ -1,5 +1,27 @@
 const router = require("express").Router();
 const Subscription = require('../models/subscriptions/subscriptions.js')
+const Shelter = require('../models/shelters/shelters.js')
+
+
+function shelterValidation (req, res, next) {
+    if (req.params.id) {
+    Shelter.getById(req.params.id)
+    .then(shelter => {
+        if (shelter){
+            next()
+        } else {
+            res.status(404).json({message: 'no shelter by that id' })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: "Error getting valid shelter", err: err.toString() })
+    })
+
+    } else {
+        res.status(500).json({ message: "no shelter id", error: error.toString()}) 
+    }
+} 
+
 
 router.get('/', (req, res) => {
     Subscription.getAllSubscriptions()
@@ -16,14 +38,18 @@ router.get('/:id', (req, res) => {
 
     Subscription.getSubscriptionbyID(id)
     .then(sub => {
+        if(req.params.id) {
         res.status(200).json(sub)
+        } else {
+            res.status(404).json({message: 'id cannot be found'})
+        }
     })
     .catch(err => {
         res.status(500).json({ message: "Error getting subscription", err: err.toString()})   
      })
 })
 
-router.get('/:id/shelter', (req, res) => {
+router.get('/:id/shelter', shelterValidation, (req, res) => {
     const { id } = req.params;
 
     Subscription.getSubscriptionbyShelter(id)
@@ -39,43 +65,55 @@ router.get('/:id/shelter', (req, res) => {
 router.get('/:id/level', (req ,res) =>{
     const { id } = req.params;
 
-    Subscription.getSubscriptionbyLevel(id)
-    .then(sub => {
-        res.status(200).json(sub)
+        Subscription.getSubscriptionbyLevel(id)
+        .then(sub => {
+            if (req.params.id){
+                res.status(200).json(sub)
+            } else {
+                res.status(404).json({message: 'id cannot be found'})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Error getting subscription", err: err.toString()})
+        })
     })
-    .catch(err => {
-        res.status(500).json({ message: "Error getting subscription", err: err.toString()})
-    })
-})
-
-
+    
+    
 router.post('/', (req, res) => {
     
     const { shelter_id, subscription_id, expiration_date, is_active } = req.body
-
-    Subscription.addSubscription({ shelter_id, subscription_id, expiration_date, is_active })
-    .then(sub => {
-        res.status(201).json({ message: "Successfully subscribed", sub})
-    })
-    .catch(err => {
-        res.status(500).json({ message: "Error subscribing", err: err.toString() })
-    })
     
+    if (req.body.shelter_id && req.body.subscription_id && req.body.expiration_date && is_active){
+        Subscription.addSubscription({ shelter_id, subscription_id, expiration_date, is_active })
+        .then(sub => {
+            res.status(201).json({ message: "Successfully subscribed", sub})
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Error subscribing", err: err.toString() })
+        })
+    } else {
+        res.status(400).json({ message: 'Required fields are incomplete' })
+    }
+        
 })
 
 router.put('/:id', (req, res) => {
     
     const { id } = req.params
     const { shelter_id, subscription_id, expiration_date, is_active } = req.body
+      
+        Subscription.updateSubscription(id, { shelter_id, subscription_id, expiration_date, is_active })
+        .then(sub => {
+            if (req.params.id){
+            res.status(200).json({ message: "Successfully updated", sub})
+            } else {
+                res.status(404).json({message: 'id cannot be found'})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Error getting updated", err: err.toString() })
+        })
 
-    Subscription.updateSubscription(id, { shelter_id, subscription_id, expiration_date, is_active })
-    .then(sub => {
-        res.status(200).json({ message: "Successfully updated", sub})
-    })
-    .catch(err => {
-        res.status(500).json({ message: "Error getting updated", err: err.toString() })
-    })
-    
 })
 
 module.exports = router;
