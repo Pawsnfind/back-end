a45a16199e1801a9e4e7c4e278cd4885ec363ebc
cmr629 <37821010+cmr629@users.noreply.github.com>
2019-07-23 20:20:37 -0400

    Update server.js

--- api/server.js
+++ api/server.js
@@ -23,7 +23,7 @@ const iSpeciesRouter = require("../routes/internal/species.js");
 const iStatesRouter = require("../routes/internal/states.js");
 const iSubscriptionsRouter = require("../routes/internal/subscriptions.js");
 
-app.use(function(req, res, next) {
+server.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
