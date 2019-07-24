b0ca35fc69f579fade75d313ed903f3bc85fa1f6
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-07-24 15:39:54 -0700

    added get all route and model for additional tables in animals

---
+++ models/internal-tables/all_options.js
@@ -0,0 +1,64 @@
+const db = require('../../data/dbConfig')
+
+module.exports = {
+    getAllOptions
+}
+
+function getAllOptions() {
+
+    let sizeQuery = db
+    .select('*')
+    .from('size')
+
+    let coatLengthQuery = db
+    .select('*')
+    .from('coat_length')
+
+    let subscriptionsQuery = db
+    .select('*')
+    .from('subscriptions')
+
+    let breedsQuery = db
+    .select('*')
+    .from('breeds')
+
+    let agesQuery = db
+    .select('*')
+    .from('ages')
+
+    let applicationStatusQuery = db
+    .select('*')
+    .from('application_status')
+
+    let speciesQuery = db
+    .select('*')
+    .from('species')
+
+    let animalStatusQuery = db
+    .select('*')
+    .from('animal_status')
+
+    let rolesQuery = db
+    .select('*')
+    .from('roles')
+
+    const promises = [sizeQuery, coatLengthQuery, subscriptionsQuery, breedsQuery, agesQuery, applicationStatusQuery, speciesQuery, animalStatusQuery, rolesQuery]
+    return Promise.all(promises).then(results => {
+       
+        let [size, coat_length, subscriptions, breeds, ages, application_status, species, animal_status, roles] = results
+        
+        return {
+            size: size,
+            coat_length: coat_length,
+            subscriptions: subscriptions,
+            breeds: breeds,
+            ages: ages,
+            application_status: application_status,
+            species: species,
+            animal_status: animal_status,
+            roles: roles
+        }
+    })
+
+}
+
