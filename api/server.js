2197c5e1c133d6487caf701536628ba5dbd3d987
cmr629 <37821010+cmr629@users.noreply.github.com>
2019-08-01 07:13:00 -0400

    Update server.js

--- api/server.js
+++ api/server.js
@@ -32,12 +32,7 @@ server.use(cors({ credentials: true, origin: '*' }))
 server.use(helmet());
 server.use(express.json());
  
-server.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+ 
 
 server.use("/api/animals", animalRouter);
 server.use("/api/applications", applicationRouter);
